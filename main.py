7c97e16894b076a94bb7b46a6ff940401994b1cc
RexRakete <johannes-stadler@web.de>
2024-09-12 21:39:49 +0200

    add filtering for alphabetical characters

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     counted_characters = count_characters(text)
+    alphabetic_characters = filter_alphabetic_characters(counted_characters)
     print(text)
     print(f"The number of words is: {num_words}")
     print(counted_characters)
+
+#convert into a list of dictionaries
+def convert_to_list_of_dicts(alphabetic_characters):
+    list_of_dicts = []
+    for character, count in alphabetic_characters.items():
+        list_of_dicts.append({"character": character, "num": count})
+    return list_of_dicts
+
+#filter out non-alphabetic characters
+def filter_alphabetic_characters(counted_characters):
+    alphabetic_characters = {}
+    for key in counted_characters:
+        if key.isalpha() == True:
+            alphabetic_characters[key] = counted_characters[key]
+    return alphabetic_characters
+            
+    
 
 # returns the number of times each character appears
 def count_characters(text):

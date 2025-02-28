7340e3906cdb6ca01ae7661fd3ac5441de2fe314
RexRakete <johannes-stadler@web.de>
2025-03-01 00:00:08 +0100

    updated bookbot

--- main.py
+++ main.py
@@ -1,68 +1,41 @@
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
-    num_words = count_words(text)
-    counted_characters = count_characters(text)
-    alphabetic_characters = filter_alphabetic_characters(counted_characters)
-    list_of_dicts = convert_to_list_of_dicts(alphabetic_characters)
-    list_of_dicts.sort(reverse=True, key=sort_on)
-
-    report_lines = [
-        f"--- Begin report of {book_path} ---",
-        f"{num_words} words found in the document",
-        ""
-    ]
-
-    for item in list_of_dicts:
-        report_lines.append(f"The '{item['character']} character was found {item['num']} times")
-
-    report_lines.append("--- End report ---")
-
-    print("\n".join(report_lines))
-
-#convert into a list of dictionaries
-def convert_to_list_of_dicts(alphabetic_characters):
-    list_of_dicts = []
-    for character, count in alphabetic_characters.items():
-        list_of_dicts.append({"character": character, "num": count})
-    return list_of_dicts
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
-def sort_on(dict):
-    return dict["num"]
-
-
-
-#filter out non-alphabetic characters
-def filter_alphabetic_characters(counted_characters):
-    alphabetic_characters = {}
-    for key in counted_characters:
-        if key.isalpha() == True:
-            alphabetic_characters[key] = counted_characters[key]
-    return alphabetic_characters
-            
-    
-
-# returns the number of times each character appears
-def count_characters(text):
-    counted_characters = {}
-    # convert any character to lowercase
-    lowered_text = text.lower()
-    for character in lowered_text:
-        if character in counted_characters:
-            counted_characters[character] += 1
-        else: counted_characters[character] = 1
-    return counted_characters
-
-
-# reads the book
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
-# counts the words
-def count_words(text):
-    return len(text.split())
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
